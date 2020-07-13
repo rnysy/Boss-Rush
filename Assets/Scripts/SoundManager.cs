29f0e936c78bd9bc9e88394c301833cdf67d50bd
HuJub <rnysy22@gmail.com>
2020-07-13 14:59:34 +0900

    체력바 버그 픽스

---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class SoundManager : MonoBehaviour
+{
+    public static SoundManager soundManager;
+
+    AudioSource gameSound;
+
+    public AudioClip hitEnemy;
+    public AudioClip hitPlayer;
+    public AudioClip selectButton;
+    public AudioClip playerJump;
+    public AudioClip playerAttack;
+    public AudioClip enemyAttack;
+    public AudioClip playerRun;
+
+    void Awake()
+    {
+        if(soundManager == null)
+        {
+            soundManager = this;
+        }
+    }
+
+    public void playHitSound()
+    {
+        gameSound.PlayOneShot(hitPlayer);
+    }
+
+    public void playEnemyHitSound()
+    {
+        gameSound.PlayOneShot(hitEnemy);
+    }
+
+    public void jumpSound()
+    {
+        gameSound.PlayOneShot(playerJump);
+    }
+
+    public void buttonSelectSound()
+    {
+        gameSound.PlayOneShot(selectButton);
+    }
+
+    public void playerAttackSound()
+    {
+        gameSound.PlayOneShot(playerAttack);
+    }
+
+    public void enemyAttackSound()
+    {
+        gameSound.PlayOneShot(enemyAttack);
+    }
+
+    public void playerRundSound()
+    {
+        gameSound.PlayOneShot(playerRun);
+    }
+}
